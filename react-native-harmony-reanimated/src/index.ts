fdf5b8c22668889a0586a3728715093848ecfe93
wangzhi <wangzhi@cainiaomeishi.com>
2024-12-10 11:58:09 +0800

    同步react-native-Harmony-reanimated逻辑

--- react-native-harmony-reanimated/src/index.ts
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import * as Animated from 'react-native-reanimated/src/Animated';
-export * from './reanimated2/NativeReanimated/NativeReanimated'
-export * from 'react-native-reanimated/src/reanimated2';
-
-global._REANIMATED_VERSION_JS = "3.6.0";
-
-export default Animated;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+import * as Animated from './react-native-reanimated/src/Animated';
+export * from './reanimated2/NativeReanimated/NativeReanimated'
+export * from './react-native-reanimated/src/reanimated2';
+
+global._REANIMATED_VERSION_JS = "3.6.0";
+
+export default Animated;
